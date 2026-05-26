d075fb5c82db4683689bc3156589536a513aab27
Sergio629 <utti232023@utvco.edu.mx>
2026-05-26 07:42:36 -0600

    se agrego la tabla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard';
+import { ProductsComponent } from './pages/products/products';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+  path: 'products',
+  component: ProductsComponent
   },
   {
     path: '**',
