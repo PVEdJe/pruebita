8431f73098d07a8ed8ca601968d8410bdd49949c
Sergio629 <utti232023@utvco.edu.mx>
2026-05-28 08:38:36 -0600

    Se corrigio el boton de products y se creo la pagina de cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { ProductsComponent } from './pages/products/products';
 import { Addproduct } from './pages/addproduct/addproduct';
+import { Users } from './pages/users/users';
+import { Addusers } from './pages/addusers/addusers';
 
 export const routes: Routes = [
   {
@@ -15,6 +17,15 @@ export const routes: Routes = [
  {
   path: 'products/add',
   component: Addproduct
+},
+{
+path: 'users',
+component: Users
+},
+{
+  path: 'users/add',
+  component: Addusers
+
 },
   {
     path: '**',
